ab569bb52238072e81ee686a2eb586ba5ef40ae0
ศศิวิมล เมืองประทุม <s6002041520131@Macmini-213-09.local>
2019-03-14 16:52:04 +0700

    edit Hello to Name

--- components/App.js
+++ components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
     render() {
         return (
             <View>
-                <Text>Hello World</Text>
+                <Text>sasiwimon muangpatum</Text>
             </View>
         );
     }
