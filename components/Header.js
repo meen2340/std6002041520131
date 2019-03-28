4c78f08d600d2bc73a840e6564c2048b63e4802f
ศศิวิมล เมืองประทุม <s6002041520131@Macmini-213-09.local>
2019-03-28 14:53:15 +0700

    decorate component with style

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,26 @@ import { View, Text } from 'react-native';
 class Header extends Component {
     render() {
         return (
-            <View>
-                <Text>{ this.props.title }</Text>
+            <View style= { styles.header }>
+                <Text style= { styles.text }>
+                    { this.props.title }
+                </Text>
             </View>
         )
+    }
+}
+const styles = {
+    header: {
+        padding: 10,
+        alignItems: 'center',
+        backgroundColor: '#FADBD8'
+        
+        
+    },
+    text: {
+        fontSize: 30,
+        color: '#2E4053'
+        
     }
 }
 
