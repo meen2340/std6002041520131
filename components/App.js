d157114d0173ceb79a0338655d46c6fa65182982
ศศิวิมล เมืองประทุม <s6002041520131@Macmini-213-09.local>
2019-03-28 15:22:41 +0700

    using Card component on App

--- components/App.js
+++ components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Header from './Header';
+import Card from './Card';
 
 class App extends React.Component {
     render() {
         return (
             <View >
                 <Header title="Albums" />
+                <Card></Card>
                 {/* <Text>sasiwimon muangpatum</Text> */}
             </View>
         );
