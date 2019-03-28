05b521a7c97bb2a284e3f82ffc5859f51f198ac8
ศศิวิมล เมืองประทุม <s6002041520131@Macmini-213-09.local>
2019-03-28 14:27:15 +0700

    using Header component on App

--- components/App.js
+++ components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from './Header';
 
 class App extends React.Component {
     render() {
         return (
             <View>
+                <Header title="Albums" />
                 <Text>sasiwimon muangpatum</Text>
             </View>
         );
