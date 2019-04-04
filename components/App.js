4dc4334b4842ec75aa3ef1cb40bda0735fc572c5
ศศิวิมล เมืองประทุม <s6002041520131@Macmini-213-09.local>
2019-04-04 17:03:06 +0700

    add component login

--- components/App.js
+++ components/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import Header from './Header';
 import Card from './Card';
+import Login from './Login';
 
 class App extends React.Component {
     render() {
         return (
             <View >
-                <Header title="Albums" />
-                <Card></Card>
+                <Header title="Login" />
+                <Login></Login>
+                {/* <Card></Card> */}
                 {/* <Text>sasiwimon muangpatum</Text> */}
             </View>
         );
